0a130ec2812424c4079d0b84d88124e4f860dca0
geoffery <geoffery009@gmail.com>
2015-02-16 11:59:19 +0800

    Create project
    SimpleLoadMoreListView

---
+++ SimpleLoadMoreListView/src/com/tom/simpleloadmorelistview/PullUptoRefreshTestActivity.java
@@ -0,0 +1,173 @@
+package com.tom.simpleloadmorelistview;
+
+import java.util.ArrayList;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tom.simpleloadmorelistview.view.LoadMoreListView;
+import com.tom.simpleloadmorelistview.view.LoadMoreListView.onLoadMoreListener;
+import com.tom.simpleloadmorelistview.view.LoadMoreListView.onPullThreadStatusListener;
+
+/**
+ * 上拉加载更多测试
+ * 
+ * @author tom
+ * @time 2015-02-16 09:35:08
+ * 
+ */
+public class PullUptoRefreshTestActivity extends Activity implements
+		onLoadMoreListener, onPullThreadStatusListener {
+	private RelativeLayout mFooterView;
+	private TextView mTextView;
+	private Thread mThread;
+	// 源数据
+	private ArrayList<String> mListItems;
+	private ArrayAdapter<String> mAdapter;
+
+	// 设置总页数
+	private int countPage = 3;
+	private int currentPage = 1;
+	private int toPage = 1;
+	private LoadMoreListView mListView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(init(this));
+	}
+
+	/**
+	 * 初始化控件
+	 * 
+	 * @param context
+	 * @return
+	 */
+	@SuppressLint("InflateParams")
+	public LoadMoreListView init(Context context) {
+		// 1.添加数据源
+		mListItems = new ArrayList<String>();
+		for (int i = 1; i <= 15; i++) {
+			mListItems.add("Item " + Integer.toString(i));
+		}
+
+		// 2.定义一个LoadMoreListView
+		mListView = new LoadMoreListView(context);
+
+		// 3.添加自己的FooterView布局
+		mFooterView = (RelativeLayout) LayoutInflater.from(
+				getApplicationContext()).inflate(R.layout.load_more_footer,
+				null);
+		mTextView = (TextView) mFooterView.findViewById(R.id.load_more_tv);
+		mAdapter = new ArrayAdapter<String>(context,
+				android.R.layout.simple_list_item_1, mListItems);
+
+		// 4.设置FooterView，必须在调用setAdapter()之前调用
+		mListView.addFooterView(mFooterView);
+
+		// 5.设置适配器
+		mListView.setAdapter(mAdapter);
+		// 6.添加加载更多监听
+		mListView.setLoadMoreListener(this);
+		// 7.添加进程状态监听
+		mListView.setThreadStatusListener(this);
+		return mListView;
+	}
+
+	@Override
+	public boolean pullUPThreadStatusIsAlive() {
+		return mThread != null && mThread.isAlive();
+	}
+
+	@Override
+	public void onLoadMore() {
+		startLoadData(toPage);
+	}
+
+	private static final int LOAD_DATA = 1;
+	private static final int LOAD_DATA_ERROR = 2;
+	@SuppressLint("HandlerLeak")
+	private Handler mHandler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			if (msg.what == LOAD_DATA) { // 成功
+				for (int i = 0; i < 5; i++) {
+					mListItems.add("item new " + i);
+				}
+				mAdapter.notifyDataSetChanged();
+
+				// 成功，请求下一页
+				if (countPage >= toPage + 1) {
+					toPage++;
+				} else {
+					mListView.setCanLoadMore(false); // 加载结束
+					Toast.makeText(getApplicationContext(), "加载完成",
+							Toast.LENGTH_SHORT).show();
+				}
+
+			} else if (msg.what == LOAD_DATA_ERROR) { // 失败
+				Toast.makeText(getApplicationContext(), "加载出错",
+						Toast.LENGTH_SHORT).show();
+				// 加载出错设置FooterView隐藏
+				mFooterView.setVisibility(View.GONE);
+
+				// 失败，重新请求
+				toPage = countPage;
+			}
+
+		};
+	};
+
+	/**
+	 * 开始加载数据方法
+	 * 
+	 * @param toPage
+	 *            请求页数
+	 */
+	private void startLoadData(int toPage) {
+		mTextView.setText("正在加载..." + "第" + toPage + "页");
+		mThread = new Thread() {
+			public void run() {
+				try {
+					// 为了测试延时效果
+					sleep(5000);
+					// throw new NullPointerException(); //出错测试
+					getResult();
+				} catch (Exception e) {
+					e.printStackTrace();
+					getResultError();
+				}
+			}
+		};
+		mThread.start();
+	}
+
+	/**
+	 * 获取数据处理
+	 */
+	private void getResult() {
+		currentPage = toPage;
+		Message msg = mHandler.obtainMessage();
+		msg.what = LOAD_DATA;
+		mHandler.sendMessage(msg);
+	}
+
+	/**
+	 * 获取数据错误处理
+	 */
+	private void getResultError() {
+		currentPage = toPage;
+		Message msg = mHandler.obtainMessage();
+		msg.what = LOAD_DATA_ERROR;
+		mHandler.sendMessage(msg);
+	}
+}
---
+++ SimpleLoadMoreListView/src/com/tom/simpleloadmorelistview/view/LoadMoreListView.java
@@ -0,0 +1,172 @@
+package com.tom.simpleloadmorelistview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ListView;
+
+/**
+ * Pull up to load more data for ListView </br>上拉加载更多控件
+ * 
+ */
+public class LoadMoreListView extends ListView implements OnScrollListener {
+
+	/**
+	 * allow to pull up to load more data
+	 */
+	private boolean pullUpLoading = false;
+
+	private boolean CanLoadMore = true;
+
+	private onPullThreadStatusListener ThreadStatusListener;
+	private onLoadMoreListener LoadMoreListener;
+	private View FooterView;
+
+	public LoadMoreListView(Context context) {
+		super(context);
+		init(context);
+	}
+
+	public LoadMoreListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context);
+	}
+
+	public LoadMoreListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(context);
+	}
+
+	private void init(Context context) {
+		this.setOnScrollListener(this);
+	}
+
+	@Override
+	public void addFooterView(View v) {
+		FooterView = v;
+		FooterView.setVisibility(View.GONE);
+		super.addFooterView(v);
+	}
+
+	public onPullThreadStatusListener getThreadStatusListener() {
+		return ThreadStatusListener;
+	}
+
+	public void setThreadStatusListener(
+			onPullThreadStatusListener threadStatusListener) {
+		ThreadStatusListener = threadStatusListener;
+	}
+
+	public onLoadMoreListener getLoadMoreListener() {
+		return LoadMoreListener;
+	}
+
+	public void setLoadMoreListener(onLoadMoreListener loadMoreListener) {
+		LoadMoreListener = loadMoreListener;
+	}
+
+	/**
+	 * get FooterView ,</br> default new a FooterView if null
+	 * 
+	 * @return
+	 */
+	public View getFooterView() {
+		if (FooterView == null) {
+			LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
+					40);
+			FooterView = new View(getContext());
+			FooterView.setLayoutParams(params);
+		}
+		return FooterView;
+	}
+
+	/**
+	 * allow to load more data
+	 * @return
+	 */
+	private boolean isCanLoadMore() {
+		return CanLoadMore;
+	}
+
+	/**
+	 * set can load more data
+	 * 
+	 * @param canLoadMore
+	 */
+	public void setCanLoadMore(boolean canLoadMore) {
+		CanLoadMore = canLoadMore;
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem,
+			int visibleItemCount, int totalItemCount) {
+		/**
+		 * when scroll to final item,and set to can pull up
+		 */
+		pullUpLoading = (firstVisibleItem + visibleItemCount == totalItemCount)
+				&& totalItemCount > 0;
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		// allow to pull up
+		if (pullUpLoading && isCanLoadMore()) {
+			setFooterViewVisible(); // here set FooterView is visible to show loading status on pages
+
+			if (!getThreadStatusListener().pullUPThreadStatusIsAlive()) { // loading data thread is alive
+				if (pullUpLoading
+						&& scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
+					// start to load data
+					if (getLoadMoreListener() != null) { // listener is not null
+						getLoadMoreListener().onLoadMore();
+					}
+				}
+			} else {
+				// Waiting load
+			}
+		} else {
+			setFooterViewGone();
+		}
+	}
+	
+	/**
+	 * set FooterView is visible
+	 */
+	private void setFooterViewVisible(){
+		getFooterView().setVisibility(View.VISIBLE);
+		getFooterView().setPadding(0, 0, 0, 0);
+	}
+	
+	/**
+	 * set FooterView is gone, and exists a blank area instead of the view, so set padding
+	 */
+	private void setFooterViewGone(){
+		getFooterView().setVisibility(View.GONE); 
+		getFooterView().setPadding(0, -getFooterView().getHeight(), 0, 0);
+	}
+
+	/**
+	 * on listener to load data thread
+	 */
+	public static interface onPullThreadStatusListener {
+		/**
+		 * check the thread status for pulling up to load data
+		 * 
+		 * @return true,refer to current thread is alive;
+		 */
+		public abstract boolean pullUPThreadStatusIsAlive();
+	}
+
+	/**
+	 * on listener to load more data
+	 * 
+	 */
+	public static interface onLoadMoreListener {
+		/**
+		 * load more data, also can set footer view status
+		 */
+		public abstract void onLoadMore();
+	}
+}
